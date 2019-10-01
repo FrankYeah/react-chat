2684531fc82298c13e6ed8fdcfd3a18795c23f64
FrankYeah <aaa24295234@gmail.com>
2019-10-01 14:33:09 +0800

    todo list

--- src/index.js
+++ src/index.js
@@ -3,22 +3,19 @@ import ReactDOM from "react-dom";
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import todoApp from './reducers'
+import App from './components/App'
 import "./styles.css";
 import "./styles.scss";
 
 let store = createStore(todoApp)
 
-class App extends React.Component {
-  render() {
-    return <div>Hello {this.props.name}</div>;
-  }
-}
+
 
 var mountNode = document.getElementById("app");
 
 ReactDOM.render(
   <Provider store={store}>
-    <App name="Frank"/>
+    <App/>
   </Provider>,
   mountNode
   );
